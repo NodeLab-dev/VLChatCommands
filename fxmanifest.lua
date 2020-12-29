abca8f9212c81f157dcfdc247a955f836fc021f2
VOLTLab <76522471+VOLTLab1@users.noreply.github.com>
2020-12-29 16:48:13 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'VOLTLab'
+description 'Roleplay Chat Commands'
+version '1.0.0'
+
+client_script './client/client.lua'
+
+server_script './server/server.lua'
